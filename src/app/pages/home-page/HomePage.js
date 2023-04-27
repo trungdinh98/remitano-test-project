da5513f1c7d88db0d9fde1d643e2c2138b11e5f0
trungdinh <batrungdinh33@gmail.com>
2023-04-27 09:57:42 +0700

    Homepage

--- src/app/pages/home-page/HomePage.js
+++ src/app/pages/home-page/HomePage.js
@@ -1,8 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './style.scss';
 
+const YOUTUBE_API_KEY = 'AIzaSyCY_cIummVhoQCRPK3hLUIiIrOdiuhagqg';
+
 const HomePage: React.FC = () => {
-  return <div className="home-page">This is HomePage</div>;
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(
+        'https://www.googleapis.com/youtube/v3/videos',
+        {
+          params: {
+            key: YOUTUBE_API_KEY,
+            part: 'snippet,statistics',
+            chart: 'mostPopular',
+            maxResults: 10,
+          },
+        }
+      );
+      setVideos(response.data.items);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      {videos.map((video) => (
+        <div key={video.id}>
+          <h2>{video.snippet.title}</h2>
+          <p>{video.snippet.description}</p>
+          <p>Views: {video.statistics.viewCount}</p>
+          <p>Likes: {video.statistics.likeCount}</p>
+          <p>Dislikes: {video.statistics.dislikeCount}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default HomePage;
